68f47ff251f244eb8304cc77ea496fd819d75090
Aditya <adidada023@gmail.com>
2022-01-19 23:44:26 +0530

    second

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View , Text } from "react-native";
+import Search_Bar from "./Search_Bar";
 
 function SearchScreen(){
     return (
         <View>
+            <Search_Bar></Search_Bar>
             <Text>Search screen</Text>
         </View>
     );
---
+++ src/Search_Bar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View , TextInput } from "react-native";
+
+function Search_Bar(){
+    return(
+        <View>
+            <TextInput placeholder = 'Enter Word' ></TextInput>
+        </View>
+    )
+}
+
+export default Search_Bar;
