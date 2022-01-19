2f8e28ac818187ace81944dd6f54227d9c1bc59d
Aditya <adidada023@gmail.com>
2022-01-20 00:50:27 +0530

    searchbar added first

--- src/Search_Bar.js
+++ src/Search_Bar.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { View , TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons';
 
-function Search_Bar(){
-    return(
-        <View>
-            <TextInput placeholder = 'Enter Word' ></TextInput>
+
+function Search_Bar() {
+    return (
+        <View style={styles.searchContainer}>
+            <Feather name="search" style={styles.searchIcon}></Feather>
+            <TextInput
+                placeholder='Enter Word'
+                style = {styles.searchInput}
+            ></TextInput>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        flexDirection: "row",
+        marginHorizontal: 30,
+        marginTop: 15,
+        borderRadius:10,
+        backgroundColor:'#F0EEEE',
+        height:45
+    },
+    searchIcon: {
+        alignSelf: 'center',
+        fontSize: 25
+    },
+    searchInput: {
+        flex:1,
+        justifyContent:'center',
+        marginLeft:10,
+        paddingLeft:10,
+        fontSize:20
+    }
+})
 
 export default Search_Bar;
